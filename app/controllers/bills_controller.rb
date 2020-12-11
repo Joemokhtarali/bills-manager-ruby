f7ea29bf7b8f897a521a4dc0db9275d29ee00481
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-12-11 15:52:09 -0500

    bills_controller complete

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,2 +1,41 @@
 class BillsController < ApplicationController
+    def index 
+        bills = Bill.all 
+        render json: bills
+    end 
+
+    def show 
+        bill = Bill.find(params[:id])
+        render json: bill 
+    end 
+
+
+    def create 
+        bill = Bill.new(bill_params)
+        if bill.save
+            render json: bill 
+        else 
+            render json: {bill.errors.full_messages}
+        end 
+
+    end 
+
+    def edit 
+        bill = Bill.find(params[:id])
+    end 
+
+    def update 
+        bill = Bill.find(params[:id])
+        bill.update(bill_params)
+    end 
+
+    def destroy
+        bill = Bill.find(params[:id])
+        bill.destroy
+    end 
+
+    private
+    def bill_params
+        params.require(:bill).permit(:name, :amount, :category, :date, :user_id)
+    end
 end
