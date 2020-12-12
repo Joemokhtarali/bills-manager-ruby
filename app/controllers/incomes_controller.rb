a35f8296c3a87edd1449e87aa3e064c46ec56fac
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-12-12 18:43:57 -0500

    incomes controller done

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,2 +1,35 @@
 class IncomesController < ApplicationController
+    def index 
+        incomes = Income.all 
+        render json: incomes 
+    end 
+
+    def show 
+        income = Income.find(params[:id])
+        render json: income 
+    end 
+
+    def create 
+        income = Income.new(income_params)
+        if income.save 
+            render json: income 
+        else 
+            render json: {income.errors.full_messages}
+        end 
+    end 
+
+    def update 
+        income = Income.find(params[:id])
+        income.update(income_params)
+    end 
+
+    def destroy 
+        income = Income.find(params[:id])
+        income.destroy 
+    end 
+
+    private 
+    def income_params
+        params.require(:income).permit(:name, :amount, :date,:category, :user_id)
+    end 
 end
